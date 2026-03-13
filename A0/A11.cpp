860b75df774b05667c75a92625696bbd6951eaac
ryugoshimizu <mayoyakisoba99@gmail.com>
2026-03-13 14:23:39 +0900

    A11 compketed

--- A0/A11.cpp
+++ A0/A11.cpp
@@ -1,10 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
+search(vector<int> A, int N, int X){
+    int l = 0;
+    int r = N - 1;
+
+    while(l <= r){
+        int k = (l + r) / 2;
+
+        if(A[k] == X){
+            cout << "A[" << k << "] の値が " << X << " になっています。" << endl;
+            return;
+        }
+        else if(A[k] < X){
+            l = k + 1;
+        }
+        else{
+            r = k - 1;
+        }
+    }
+}
 
 int main(){
-    int N X;
-    for (int i = 0;i < N; i++){
+    int N, X;
+    cin >> N >> X;
+    vector<int> A(N);
+    for(int i = 0; i < N; i++){
         cin >> A[i];
     }
-    
+    search(A, N, X);
 }
